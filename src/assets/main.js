2987a181293f6394a221bb419433530f3476056d
CoreyDeRosa <coreyderosa@gmail.com>
2017-11-10 16:34:55 -0700

    Make an AJAX call
    made my first AJAX calllllllllllllllllllllllllll

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
-
-  // your code will go here
+  var myBadgesURI = 'https://www.codeschool.com/users/gowenburnett.json'
+  $.ajax({
+    url: myBadgesURI,
+    dataType: 'jsonp',
+    success: function(response){
+      console.log("Success!  You made you first freakin' AJAX call!");
+      console.log("Derp!  You made you first freakin' AJAX call!");
+      console.log(response);
+      console.log(response);
+    }
+  })
+    
+    // your code will go here
 
 });
