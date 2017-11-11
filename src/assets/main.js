81ffecd299c882bec07644561c5f356a974278bd
coreyderosa <coreyderosa@gmail.com>
2017-11-10 23:42:43 -0700

    An Element Per Completed Course
    finished step 6 in the jQueryBadgesProject....looking good.

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,12 +5,30 @@ $(function() {
     dataType: 'jsonp',
     success: function(response){
       console.log("Success!  You made you first freakin' AJAX call!");
-      console.log("Derp!  You made you first freakin' AJAX call!");
-      console.log(response);
-      console.log(response);
+      console.log("Full AJAX response: " + response);
+      console.log("Title of a course: " + response.courses.completed[3].title);
+      var completedCourse = (response.courses.completed);
+      console.log("Practice: " + completedCourse.length);
+
+      for(var i = 0; i < completedCourse.length; i++){
+        console.log("for loop course title: " + completedCourse[i].title);
+
+        titleCompletedCourse = completedCourse[i].title
+
+        var divElement = document.createElement("div");
+        var addClass = document.getElementById("badges").classList;
+        addClass.add("course");
+
+        var testText = document.createTextNode(titleCompletedCourse);
+        console.log(testText);
+        divElement.appendChild(testText);
+
+        document.getElementById("badges").appendChild(divElement);  
+      }
     }
   })
     
     // your code will go here
 
 });
+
