3c04897e37ce158affb778db96874e1930fcb4db
coreyderosa <coreyderosa@gmail.com>
2017-11-11 00:55:37 -0700

    added course images
    finished step 8- added badge images to each div

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,34 +5,54 @@ $(function() {
     dataType: 'jsonp',
     success: function(response){
       console.log("Success!  You made you first freakin' AJAX call!");
-      console.log("Full AJAX response: " + response);
-      console.log("Title of a course: " + response.courses.completed[3].title);
+      console.log("Full AJAX response: ",  response);
+      console.log("Title of a course: ", response.courses.completed[3].title);
       var completedCourse = (response.courses.completed);
       console.log("Practice: " + completedCourse.length);
 
       for(var i = 0; i < completedCourse.length; i++){
-        console.log("for loop course title: " + completedCourse[i].title);
+        //console.log("for loop course title: " + completedCourse[i].title);
 
         var titleCompletedCourse = completedCourse[i].title
-        var divElement = document.createElement("div"); //creates each div
-        divElement.className = "course"; //add class="course" to each div
-        
-        
-        var titleText = document.createTextNode(titleCompletedCourse);
-        console.log(titleText);
-        //divElement.appendChild(titleText);
-        document.getElementById("badges").appendChild(divElement);  
 
+        //creates each div
+        var divElement = document.createElement("div"); 
+
+        //add class="course" to each div
+        divElement.className = "course"; 
+        
+        //grab title from AJAX call
+        var titleText = document.createTextNode(titleCompletedCourse);
+        //console.log(titleText);
+
+        //id="badges" variable
         var badgesElement = document.getElementById("badges");
+
+        //add divs to id="badges" div
+        badgesElement.appendChild(divElement);  
+        
+        //creates each h3
         var hThreeElement = document.createElement("h3");
 
+        //adds h3 element to each div
         divElement.appendChild(hThreeElement);
-        hThreeElement.appendChild(titleText);
-        //var hThreeElement = document.createElement("h3");
-        //var courseClass = document.getElementsByClassName("course").appendChild(hThreeElement);
-        
-        
 
+        //adds title to h3 element
+        hThreeElement.appendChild(titleText);
+
+        //creates img element
+        var imageElement = document.createElement("img");
+
+        //add img to class="course" divs
+        divElement.appendChild(imageElement);
+
+        //src = response.badge
+        var badgeImageURL = response.courses.completed[i].badge;
+        console.log(badgeImageURL); //returns badge image url
+
+        //set src = badge image URL
+        imageElement.setAttribute("src", badgeImageURL);
+ 
       }
     }
   })
